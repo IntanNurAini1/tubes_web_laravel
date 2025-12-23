d72d608d7a37fcf14e27eff03520acec6ce44694
Viornette <lucieleason@gmail.com>
2025-12-24 04:00:44 +0700

    Menambahkan API Read Karyawan

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Api\KaryawanController;
 
 Route::get('/', [ProductController::class, 'index']);
 
@@ -10,3 +11,7 @@ Route::post('/store', [ProductController::class, 'store']);
 Route::post('/update/{kode}', [ProductController::class, 'update']);
 
 Route::get('/delete/{kode}', [ProductController::class, 'delete']);
+
+Route::get('/api/employees-php', [KaryawanController::class, 'index']);
+
+Route::get('/api/employees-php/{nip}', [KaryawanController::class, 'show']);
