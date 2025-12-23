2bfbf3b3a4b01ad8ffdfb954649c2e1d508473ba
IntanNurAini <intan.nuraini2005@gmail.com>
2025-12-24 04:46:10 +0700

    fix product

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::get('/', [ProductController::class, 'index']);
 
-Route::post('/store', [ProductController::class, 'store']);
-
-Route::post('/update/{kode}', [ProductController::class, 'update']);
-
-Route::get('/delete/{kode}', [ProductController::class, 'delete']);
+Route::get('/produk', [ProductController::class, 'index']);
+Route::post('/produk/store', [ProductController::class, 'store']);
+Route::post('/produk/update/{kode}', [ProductController::class, 'update']);
+Route::get('/produk/delete/{kode}', [ProductController::class, 'delete']);
