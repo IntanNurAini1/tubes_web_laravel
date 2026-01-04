8e1e11229668da25807d9dcfa34df9537758c044
IntanNurAini <intan.nuraini2005@gmail.com>
2026-01-05 05:15:04 +0700

    updd

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,22 +8,78 @@ use Illuminate\Support\Facades\DB; // WAJIB
 
 class ProductController extends Controller
 {
-    // private $api = 'http://localhost:3000/api/products';
-
-    // public function index()
-    // {
-    //     $response = Http::get($this->api);
-    //     $products = $response->json();
-
-    //     return view('produk', compact('products'));
-    // }
+    private $api = 'http://localhost:3000/api/products';
 
     public function index()
     {
-        $products = DB::table('products')->get();
+        $response = Http::get($this->api);
+        $products = $response->json();
 
         return view('produk', compact('products'));
     }
+
+    // public function store(Request $request)
+    // {
+    //     $request->validate([
+    //         'kode_produk' => [
+    //             'required',
+    //             'regex:/^P[0-9]{3}$/',
+    //         ],
+    //         'nama_produk' => 'required',
+    //         'jumlah'      => 'required|numeric',
+    //         'status'      => 'required'
+    //     ]);
+
+    //     $products = Http::get($this->api)->json();
+
+    //     $exists = collect($products)
+    //         ->contains('kode_produk', $request->kode_produk);
+
+    //     if ($exists) {
+    //         return back()
+    //             ->withErrors(['kode_produk' => 'Kode produk tidak boleh sama'])
+    //             ->withInput();
+    //     }
+
+    //     Http::post($this->api, [
+    //         'kode_produk' => $request->kode_produk,
+    //         'nama_produk' => $request->nama_produk,
+    //         'jumlah'      => $request->jumlah,
+    //         'status'      => $request->status
+    //     ]);
+
+    //     return redirect('/produk')->with('success', 'Produk berhasil ditambahkan');
+    // }
+
+    // public function update(Request $request, $kode)
+    // {
+    //     $request->validate([
+    //         'nama_produk' => 'required',
+    //         'jumlah'      => 'required|numeric',
+    //         'status'      => 'required'
+    //     ]);
+
+    //     Http::put($this->api.'/'.$kode, [
+    //         'nama_produk' => $request->nama_produk,
+    //         'jumlah'      => $request->jumlah,
+    //         'status'      => $request->status
+    //     ]);
+
+    //     return redirect('/produk')->with('success', 'Produk berhasil diperbarui');
+    // }
+
+    // public function delete($kode)
+    // {
+    //     Http::delete($this->api.'/'.$kode);
+    //     return redirect('/produk')->with('success', 'Produk berhasil dihapus');
+    // }
+
+    // public function index()
+    // {
+    //     $products = DB::table('products')->get();
+
+    //     return view('produk', compact('products'));
+    // }
 
     public function store(Request $request)
     {
